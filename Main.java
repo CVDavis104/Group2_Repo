137b09ec06e250f7fd2fa9159d284dff0889444f
PottorffPeter <hardrrocker22@gmail.com>
2022-06-24 10:33:42 -0400

    'ello

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args){
 
-
+        interface Animal {
+            public void animalSound(); // interface method (does not have a body)
+            public void sleep(); // interface method (does not have a body)
 
     /*main method code block ending*/}
 /*Main class code block ending*/}
